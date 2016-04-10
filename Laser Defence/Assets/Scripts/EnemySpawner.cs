c8d6bf4db4c69417a27728ac2ace8b8b9d6f98d5
yl354@duke.edu <yl354@duke.edu>
2016-04-10 02:28:24 -0400

    Initial the project & creating some enemies and player & player's laser

---
+++ Laser Defence/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+	public float width = 10f;
+	public float height = 5f;
+	public float speed = 5f;
+
+	float xmin;
+	float xmax;
+
+	private bool moveRight = true;
+
+	// Use this for initialization
+	void Start () {
+		float Zdistance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftMost = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, Zdistance));
+		Vector3 rightMost = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, Zdistance));
+		xmin = leftMost.x;
+		xmax = rightMost.x;
+
+		foreach (Transform child in transform) {
+			GameObject enemy = Instantiate (enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+			enemy.transform.parent = child;			//Make the instantiate enemy inside the EnemyFormation(Optional)
+
+		}
+	}
+
+	public void OnDrawGizmos(){
+		Gizmos.DrawWireCube (transform.position, new Vector3 (width, height, 0));
+	}
+	// Update is called once per frame
+	void Update () {
+		//The position change and the judge process could be seperated!!!
+		//There will be much more clearly!!!
+
+		if (moveRight) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
+			if (transform.position.x + width / 2 >= xmax) {
+				moveRight = false;
+			}
+		} else {
+			transform.position += Vector3.left * speed * Time.deltaTime;
+			if(transform.position.x - width / 2 <= xmin) {
+				moveRight = true;
+			}
+		}
+	}
+}
---
+++ Laser Defence/Assets/Scripts/playerController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+	public float speed = 5f;
+	public float padding = 0.5f;	//the offset in the boundry
+	public GameObject projectile;	//The bullet object
+	public float laserSpeed = 5f;
+	private float lastFireTime;
+	public float fireRate = 1f;
+
+	float xmin;
+	float xmax;
+	float ymin;
+	float ymax;
+
+	// Use this for initialization
+	void Start () {
+		float Zdistance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftMost = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, Zdistance));	//The left-most position
+		Vector3 rightMost = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, Zdistance));	//The right-most position
+		Vector3 topMost = Camera.main.ViewportToWorldPoint (new Vector3 (0, 1, Zdistance));		//The top-most position
+		Vector3 bottomMost = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, Zdistance));	//The bottom-most position
+		xmin = leftMost.x + padding;
+		xmax = rightMost.x - padding;
+		ymin = bottomMost.y + padding;
+		ymax = topMost.y - padding;
+	}
+
+	void Fire(){
+		lastFireTime = Time.time;
+		GameObject laser = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+		laser.rigidbody2D.velocity = new Vector2(0, laserSpeed);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		//Update the player's position
+		Vector3 playerPosition = this.transform.position;
+		if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
+			playerPosition += Vector3.up * Time.deltaTime * speed;  
+		}
+		if (Input.GetKey (KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
+			playerPosition += Vector3.down * Time.deltaTime * speed;  
+		}
+		if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
+			playerPosition += Vector3.left * Time.deltaTime * speed;  
+		}
+		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+			playerPosition += Vector3.right * Time.deltaTime * speed;  
+		}
+
+		//Instantiate the bullet object
+		if (Input.GetKey (KeyCode.Space) || Input.GetMouseButton (0)) {
+			if(Time.time - lastFireTime >= fireRate){
+				Fire ();
+			}
+		}
+
+		this.transform.position = new Vector3(Mathf.Clamp(playerPosition.x, xmin, xmax), Mathf.Clamp(playerPosition.y, ymin, ymax), playerPosition.z);	//Restriction of the player's position
+
+	}
+}
