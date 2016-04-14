3843cc350d90afa70bab971074619cef7d8a2e0e
yl354@duke.edu <yl354@duke.edu>
2016-04-14 14:46:07 -0400

    Instatiate the powerup item every 3 seconds

---
+++ Laser Defence/Assets/GameMaster.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour {
+
+
+	public GameObject PowerUpPrefab;
+	public float PowerUpSpawnTime = 3.0f;
+	private bool isSpawn = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (Time.timeSinceLevelLoad);
+		if (Time.timeSinceLevelLoad >= PowerUpSpawnTime && isSpawn == false) {
+			Instantiate(PowerUpPrefab, this.transform.position, Quaternion.identity);
+			isSpawn = true;
+		}
+	}
+}
